eb82914d9679f5523121cecc62fe5cc3ca9c0319
amelie certin <amelie.certin@gmail.com>
2017-05-26 15:47:26 +0200

    Create user on chiepherd_api

--- src/main/java/chiepherd/controllers/SignUpController.kt
+++ src/main/java/chiepherd/controllers/SignUpController.kt
@@ -4,9 +4,9 @@ import com.jfoenix.controls.JFXPasswordField
 import com.jfoenix.controls.JFXTextField
 import javafx.event.ActionEvent
 import javafx.fxml.FXML
-import javafx.fxml.Initializable
 import java.net.URL
 import java.util.*
+import khttp.post
 
 class SignUpController : ApplicationController() {
     @FXML lateinit var first_name            : JFXTextField
@@ -21,6 +21,16 @@ class SignUpController : ApplicationController() {
 
     @FXML fun onSignIn(actionEvent : ActionEvent?) {
         if(actionEvent == null) { return }
-        switchScene(actionEvent, "chiepherd/views/Login.fxml")
+        val payload = mapOf("email"                 to email.text,
+                            "password"              to password.text,
+                            "firstname"             to first_name.text,
+                            "lastname"              to last_name.text,
+                            "password_confirmation" to password_confirmation.text)
+        val res = post("http://192.168.56.103:3000/auth/register", data = payload)
+        if(res.statusCode != 200) {
+            displayError(res)
+        } else {
+            switchScene(actionEvent, "chiepherd/views/Login.fxml")
+        }
     }
 }
